8f51afe837a298195ed5ba59f50c74d46fb8cae4
Goldflame09 <goldflame09@gmail.com>
2014-11-17 19:46:25 -0500

    hide password

--- g2.sh
+++ g2.sh
@@ -1,3 +1,5 @@
+echo -n "Enter password: "; read PSWD
+
 cd ~/cm-12.0/.repo/local_manifests
 curl -O https://raw.githubusercontent.com/Rashed97/local_manifests/master/g2_cm-12.0.xml
 cd ~/cm-12.0
@@ -8,26 +10,26 @@ vendor/cm/get-prebuilts
 make clobber
 brunch d800
 cd out/target/product/d800
-curl -T cm-*.zip -u rashed:y80uqTxVEpDp ftp://uploads.androidfilehost.com --ftp-create-dirs
+curl -T cm-*.zip -u rashed:PSWD ftp://uploads.androidfilehost.com --ftp-create-dirs
 cd ../../../..
 rm -rf out/target/product/d800
 brunch d801
 cd out/target/product/d801
-curl -T cm-*.zip -u rashed:y80uqTxVEpDp ftp://uploads.androidfilehost.com --ftp-create-dirs
+curl -T cm-*.zip -u rashed:PSWD ftp://uploads.androidfilehost.com --ftp-create-dirs
 cd ../../../..
 rm -rf out/target/product/d801
 brunch d802
 cd out/target/product/d802
-curl -T cm-*.zip -u rashed:y80uqTxVEpDp ftp://uploads.androidfilehost.com --ftp-create-dirs
+curl -T cm-*.zip -u rashed:PSWD ftp://uploads.androidfilehost.com --ftp-create-dirs
 cd ../../../..
 rm -rf out/target/product/d802
 brunch ls980
 cd out/target/product/ls980
-curl -T cm-*.zip -u rashed:y80uqTxVEpDp ftp://uploads.androidfilehost.com --ftp-create-dirs
+curl -T cm-*.zip -u rashed:PSWD ftp://uploads.androidfilehost.com --ftp-create-dirs
 cd ../../../..
 rm -rf out/target/product/ls980
 brunch vs980
 cd out/target/product/vs980
-curl -T cm-*.zip -u rashed:y80uqTxVEpDp ftp://uploads.androidfilehost.com --ftp-create-dirs
+curl -T cm-*.zip -u rashed:PSWD ftp://uploads.androidfilehost.com --ftp-create-dirs
 cd ../../../..
 rm -rf out/target/product/vs980
